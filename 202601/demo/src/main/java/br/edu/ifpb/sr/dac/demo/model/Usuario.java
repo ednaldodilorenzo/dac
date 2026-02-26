56fe14ad0dee4ccf5208b78ebd468264c2439877
edsf <ednaldo.dilorenzo@gmail.com>
2026-02-25 21:46:06 -0300

    Adição do banco de dados.

--- 202601/demo/src/main/java/br/edu/ifpb/sr/dac/demo/model/Usuario.java
+++ 202601/demo/src/main/java/br/edu/ifpb/sr/dac/demo/model/Usuario.java
@@ -1,9 +1,6 @@
 package br.edu.ifpb.sr.dac.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Objects;
@@ -15,8 +12,11 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String nome;
+    @Column(nullable = false)
     private String username;
+    @Column(nullable = false)
     private String senha;
 
     @Override
