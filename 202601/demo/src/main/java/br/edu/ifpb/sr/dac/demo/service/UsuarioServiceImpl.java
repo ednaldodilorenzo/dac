880a50e276561d22eed935d20d242fe3b61b93d9
edsf <ednaldo.dilorenzo@gmail.com>
2026-02-26 18:34:54 -0300

    Utilização do mapstruct.

--- 202601/demo/src/main/java/br/edu/ifpb/sr/dac/demo/service/UsuarioServiceImpl.java
+++ 202601/demo/src/main/java/br/edu/ifpb/sr/dac/demo/service/UsuarioServiceImpl.java
@@ -3,9 +3,9 @@ package br.edu.ifpb.sr.dac.demo.service;
 import br.edu.ifpb.sr.dac.demo.dao.UsuarioDao;
 import br.edu.ifpb.sr.dac.demo.dto.GetUsuariosRespDTO;
 import br.edu.ifpb.sr.dac.demo.dto.PostUsuarioDTO;
+import br.edu.ifpb.sr.dac.demo.dto.UsuarioMapper;
 import br.edu.ifpb.sr.dac.demo.model.Usuario;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,27 +14,22 @@ import java.util.List;
 public class UsuarioServiceImpl implements UsuarioService {
 
     private final UsuarioDao usuarioDao;
+    private final UsuarioMapper usuarioMapper;
 
-    public UsuarioServiceImpl(UsuarioDao usuarioDao) {
+    public UsuarioServiceImpl(UsuarioDao usuarioDao, UsuarioMapper usuarioMapper) {
         this.usuarioDao = usuarioDao;
+        this.usuarioMapper = usuarioMapper;
     }
 
     @Override
     @Transactional
     public void save(PostUsuarioDTO dto) {
-        Usuario usuario = new Usuario();
-        usuario.setNome(dto.nome());
-        usuario.setSenha(dto.senha());
-        usuario.setUsername(dto.username());
+        Usuario usuario = this.usuarioMapper.toUsuarioEntity(dto);
         this.usuarioDao.save(usuario);
     }
 
     @Override
     public List<GetUsuariosRespDTO> listAll() {
-        return this.usuarioDao.
-                findAll().
-                stream().
-                map(usuario -> new GetUsuariosRespDTO(usuario.getId(), usuario.getNome(), usuario.getUsername())).
-                toList();
+        return this.usuarioDao.findAll().stream().map(usuario -> new GetUsuariosRespDTO(usuario.getId(), usuario.getNome(), usuario.getUsername())).toList();
     }
 }
