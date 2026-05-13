bf47e78ed434f3bc82ceb09a8d1a2b8e632baae2
edsf <ednaldo.dilorenzo@gmail.com>
2026-05-13 19:25:07 -0300

    [fix] Adicionar o token nas requisições pós login.

--- 202601/react-demo/src/utils/requester.ts
+++ 202601/react-demo/src/utils/requester.ts
@@ -2,4 +2,12 @@ import { create } from 'axios'
 
 export const requester = create({
     baseURL: "/api",
-})
+});
+
+requester.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
